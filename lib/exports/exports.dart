72624569c31e45b348213be5c5caaa42b7ce6dee
Mugamba669 <brunohectre@gmail.com>
2023-06-06 14:35:50 +0300

    setup mapbox

--- lib/exports/exports.dart
+++ lib/exports/exports.dart
@@ -6,6 +6,8 @@ export 'package:flutter/material.dart';
 export 'package:flutter_native_splash/flutter_native_splash.dart';
 export 'package:firebase_ui_auth/firebase_ui_auth.dart';
 export 'package:firebase_core/firebase_core.dart';
+export 'package:mapbox_gl/mapbox_gl.dart';
+export 'package:collection/collection.dart';
 // export 'package:firebase_auth/firebase_auth.dart';
 // routes
 export '/routes/Routes.dart';
